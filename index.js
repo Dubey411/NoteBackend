4b9920d2c3650cffdc5bb832a651a05403b4a463
Shubham <dubeytech9619@gmail.com>
2025-08-26 20:14:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const notesRoutes = require("./routes/notes");
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "https://your-frontend-name.vercel.app" }));
+
 app.use(express.json());
 
 // MongoDB connection
